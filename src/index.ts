dd732b88e95000a9c3e24cc6ed8a9e36c1aac9ed
Jakub Robotka <jakub.robotka@gmail.com>
2019-10-20 20:59:48 +0200

    [Fea] Carousel into project and IObserver init

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,50 @@
-const targets = Array.from(document.querySelectorAll("[data-observe]"));
+import Glide from "@glidejs/glide";
 
-const lazyLoad = target => {
+const timeline = Array.from(document.querySelectorAll("[data-observe]"));
+const sliders = Array.from(document.querySelectorAll("[data-slider]"));
+
+const lazyLoad = (target, cb, config = {}) => {
   const io = new IntersectionObserver(
     (entries, observer) => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          const target = entry.target;
-          const classe = target.getAttribute("data-classes").split(" ");
-          target.classList.remove(...classe);
-
+          cb(entry);
           observer.disconnect();
         }
       });
     },
     {
       rootMargin: "-100px",
-      threshold: 0
+      threshold: 0,
+      ...config
     }
   );
 
   io.observe(target);
 };
 
-targets.forEach(lazyLoad);
+function appendClasses(entry) {
+  const target = entry.target;
+  const classe = target.getAttribute("data-classes").split(" ");
+  target.classList.remove(...classe);
+}
+
+function initCarousel(entry) {
+  const glide = new Glide(entry.target, {
+    type: "carousel",
+    perView: 8,
+    autoplay: 1,
+    animationDuration: 3000,
+    startAt: 0,
+    animationTimingFunc: "linear"
+  });
+
+  glide.on("mount.before", () => {
+    console.log("asdfas");
+  });
+
+  glide.mount();
+}
+
+timeline.forEach(e => lazyLoad(e, appendClasses));
+sliders.forEach(e => lazyLoad(e, initCarousel, { rootMargin: "100px" }));
